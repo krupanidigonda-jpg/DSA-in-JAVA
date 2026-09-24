3380e8380afa97c6e6da6ca6d8d1755bc93023e5
NIDIGONDA KRUTHIK ROSHAN <krupanidigonda@gmail.com>
2026-09-24 19:51:01 +0530

    Time: 6 ms (9.02%) | Memory: 46.3 MB (93.21%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null || head.next==null){
+            return null;
+        }
+
+        HashSet<ListNode>hs=new HashSet<>();
+
+        ListNode temp=head;
+        while(temp!=null){
+            if(hs.contains(temp)){
+                return temp;
+            }
+            else{
+                hs.add(temp);
+                temp=temp.next;
+            }
+        }
+        return null;
+    }
+}
