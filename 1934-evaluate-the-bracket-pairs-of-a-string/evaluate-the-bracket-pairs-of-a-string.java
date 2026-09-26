791cfde68290885095ab924f53d11e15ad3774e6
NIDIGONDA KRUTHIK ROSHAN <krupanidigonda@gmail.com>
2026-09-26 21:51:36 +0530

    Time: 149 ms (6.79%) | Memory: 101.2 MB (6.79%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,31 +1,35 @@
 class Solution {
     public static String evaluate(String s, List<List<String>> knowledge) {
-        HashMap<String,String>hm=new HashMap<>();
-        for(int i=0;i<knowledge.size();i++){
-            hm.put(knowledge.get(i).get(0),knowledge.get(i).get(1));
+        HashMap<String,String> hm = new HashMap<>();
+
+        for(int i = 0; i < knowledge.size(); i++){
+            hm.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
         }
-        
-        String sb="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
+
+        String sb = "";
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
                 i++;
-                String temp="";
-                while(s.charAt(i)!=')'){
-                    temp+=s.charAt(i);
+                String temp = "";
+
+                while(s.charAt(i) != ')'){
+                    temp += s.charAt(i);
                     i++;
                 }
+
                 if(hm.containsKey(temp)){
-                    sb+=hm.get(temp);
+                    sb += hm.get(temp);
                 }
                 else{
-                    sb+="?";
+                    sb += "?";
                 }
-                
             }
             else{
-                sb+=s.charAt(i);
+                sb += s.charAt(i);
             }
         }
+
         return sb;
     }
 }
