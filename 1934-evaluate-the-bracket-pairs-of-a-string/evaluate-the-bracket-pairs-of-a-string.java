eeb49b450010a0e164988a276cd42e355925b42b
NIDIGONDA KRUTHIK ROSHAN <krupanidigonda@gmail.com>
2026-09-26 21:50:26 +0530

    Time: 151 ms (6.33%) | Memory: 101.5 MB (5.43%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static String evaluate(String s, List<List<String>> knowledge) {
+        HashMap<String,String>hm=new HashMap<>();
+        for(int i=0;i<knowledge.size();i++){
+            hm.put(knowledge.get(i).get(0),knowledge.get(i).get(1));
+        }
+        
+        String sb="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                i++;
+                String temp="";
+                while(s.charAt(i)!=')'){
+                    temp+=s.charAt(i);
+                    i++;
+                }
+                if(hm.containsKey(temp)){
+                    sb+=hm.get(temp);
+                }
+                else{
+                    sb+="?";
+                }
+                
+            }
+            else{
+                sb+=s.charAt(i);
+            }
+        }
+        return sb;
+    }
+}
